f096b5ab8484609acce1d6fafb11cf83e9eb71c5
Kyle <knicksqaz@gmail.com>
2025-05-29 13:01:45 +0800

    2025_05_29

---
+++ 2025_05_29/Tutorial 9-4/Cell Phone Inventory/Cell Phone Inventory/CellPhone.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cell_Phone_Inventory
+{
+    class CellPhone
+    {
+        // 欄位：儲存手機品牌名稱的私有變數
+        private string _brand;   // 儲存手機品牌，例如：Apple、Samsung 等
+
+        // 欄位：儲存手機型號的私有變數
+        private string _model;   // 儲存手機型號，例如：iPhone 15、Galaxy S24 等
+
+        // 欄位：儲存手機建議售價的私有變數
+        private decimal _price;  // 儲存手機的零售價格，使用 decimal 型別以確保金額精確
+
+        // 建構函式：初始化 CellPhone 物件的欄位
+        // 將品牌與型號設為空字串，價格設為 0
+        public CellPhone()
+        {
+            _brand = "";
+            _model = "";
+            _price = 0m;
+        }
+
+        // 品牌屬性：取得或設定手機品牌名稱
+        // 透過此屬性可存取或修改 _brand 欄位的值
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+
+        // 型號屬性：取得或設定手機型號
+        // 透過此屬性可存取或修改 _model 欄位的值
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        // 價格屬性：取得或設定手機價格
+        // 透過此屬性可存取或修改 _price 欄位的值
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+    }
+}
---
+++ 2025_05_29/Tutorial 9-4/Cell Phone Inventory/Cell Phone Inventory/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cell_Phone_Inventory
+{
+    public partial class Form1 : Form
+    {
+        // 用來儲存 CellPhone 物件的清單
+        List<CellPhone> phoneList = new List<CellPhone>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // GetPhoneData 方法接收一個 CellPhone 物件作為參數。
+        // 此方法會將使用者輸入的資料指派給該物件的屬性。
+        private void GetPhoneData(CellPhone phone)
+        {
+            // 用來暫存價格的變數
+            decimal price;
+
+            // 取得手機品牌
+            phone.Brand = brandTextBox.Text;
+
+            // 取得手機型號
+            phone.Model = modelTextBox.Text;
+
+            // 取得手機價格
+            if (decimal.TryParse(priceTextBox.Text, out price))
+            {
+                phone.Price = price;
+            }
+            else
+            {
+                // 顯示錯誤訊息
+                MessageBox.Show("價格無效");
+            }
+        }
+
+        private void addPhoneButton_Click(object sender, EventArgs e)
+        {
+            CellPhone myPhone = new CellPhone(); // 建立新的 CellPhone 物件
+
+            GetPhoneData(myPhone); //取得使用者輸入的手機資料
+
+            // 將新的手機物件加入到手機清單中
+            phoneList.Add(myPhone);
+
+            //將新增手機的品牌與型號組合成字串，並加入到ListBox 中
+            phoneListBox.Items.Add(myPhone.Brand + " " + myPhone.Model);
+
+            // 清空輸入欄位，準備下一次輸入
+            brandTextBox.Text = "";
+            modelTextBox.Text = "";
+            priceTextBox.Text = "";
+            // 將焦點設回品牌輸入欄位，方便使用者繼續輸入
+            brandTextBox.Focus();
+        }
+
+        private void phoneListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = phoneListBox.SelectedIndex; // 取得選取的手機在清單中的索引
+
+            MessageBox.Show(phoneList[index].Price.ToString("C"));// 顯示選取手機的售價，格式化為貨幣顯示
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單
+            this.Close();
+        }
+    }
+}
